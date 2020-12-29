f502bb2d7b94cb3deb96a9a448de58f6160539b8
GigaPok <gigapokera@gmail.com>
2020-12-29 20:27:17 +0400

    first commit

--- js/dark-mode-switch.js
+++ js/dark-mode-switch.js
@@ -15,8 +15,8 @@ window.addEventListener('load', () => {
  * the switch is 'on' or 'off'.
  *
  * Description: initTheme is a function that uses localStorage from JavaScript DOM,
- * to store the value of the HTML switch. If the switch was already switched to
- * 'on' it will set an HTML attribute to the body named: 'data-theme' to a 'dark'
+ * to store the value of the body switch. If the switch was already switched to
+ * 'on' it will set an body attribute to the body named: 'data-theme' to a 'dark'
  * value. If it is the first time opening the page, or if the switch was off the
  * 'data-theme' attribute will not be set.
  * @return {void}
@@ -26,23 +26,24 @@ function initTheme() {
     localStorage.getItem('darkSwitch') !== null &&
     localStorage.getItem('darkSwitch') === 'dark';
   darkSwitch.checked = darkThemeSelected;
-  darkThemeSelected ? document.getElementById("dark-mode2").setAttribute('data-theme', 'dark') :
-    document.getElementById("dark-mode2").removeAttribute('data-theme');
+  darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') :
+    document.body.removeAttribute('data-theme');
 }
 
 
 /**
  * Summary: resetTheme checks if the switch is 'on' or 'off' and if it is toggled
- * on it will set the HTML attribute 'data-theme' to dark so the dark-theme CSS is
+ * on it will set the body attribute 'data-theme' to dark so the dark-theme CSS is
  * applied.
  * @return {void}
  */
 function resetTheme() {
   if (darkSwitch.checked) {
-    document.getElementById("dark-mode2").setAttribute('data-theme', 'dark');
+    document.body.setAttribute('data-theme', 'dark');
     localStorage.setItem('darkSwitch', 'dark');
   } else {
-    document.getElementById("dark-mode2").removeAttribute('data-theme');
+    document.body.removeAttribute('data-theme');
     localStorage.removeItem('darkSwitch');
   }
 }
+
